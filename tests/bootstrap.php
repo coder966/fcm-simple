6163675b7808fd73db4cd6e5f589c675338e21b9
Khalid <coder966@gmail.com>
2019-05-21 06:18:13 +0300

    update CLI message to use local version of PHPUnit installed via Composer

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 $serverKey = getenv("SERVER_KEY");
 if($serverKey == false){
-    die("\n\nUsage: SERVER_KEY=<a-valid-server-key> phpunit\n\n");
+    die("\n\nUsage: SERVER_KEY=<a-valid-server-key> vendor/bin/phpunit\n\n");
 }else{
     define("SERVER_KEY", $serverKey);
 }
